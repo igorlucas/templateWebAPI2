533d216005098dcddc0add215c8db4f9e8b7533e
igorlucas <igorlucas.silva@bol.com.br>
2018-08-23 15:36:02 -0300

    init

---
+++ WebAPI2/WebApiCors/WebApiCors/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApiCors.Models;
+
+namespace WebApiCors.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private List<Product> products = new List<Product>();
+
+        public ProductsController()
+        {
+            products.Add(new Product { Id = 1, Name = "Produto 1" });
+            products.Add(new Product { Id = 2, Name = "Produto 2" });
+            products.Add(new Product { Id = 3, Name = "Produto 3" });
+            products.Add(new Product { Id = 4, Name = "Produto 4" });
+            products.Add(new Product { Id = 5, Name = "Produto 5" });
+            products.Add(new Product { Id = 6, Name = "Produto 6" });
+            products.Add(new Product { Id = 7, Name = "Produto 7" });
+            products.Add(new Product { Id = 8, Name = "Produto 8" });
+        }
+
+
+        [EnableCors(origins: "http://localhost:3094", headers: "*", methods: "*")]
+        public IList<Product> Get()
+        {
+            return products;
+        }
+    }
+}
